752dce2e6bf9717db045f5adbc17b7dce827deec
Pradip Paudel <pradippaudel2052@gmail.com>
2021-04-08 16:43:49 +0545

    Add json data and display data in card in home page

---
+++ src/components/CCard.js
@@ -0,0 +1,29 @@
+import moment from "moment";
+import React from "react";
+import { Card, ListGroup, ListGroupItem, Button } from "react-bootstrap";
+
+const CCard = ({
+  data: { id, name, image, price, stock, published_date, author, genre },
+}) => {
+  return (
+    <Card style={{ width: "15rem", margin: 6 }}>
+      <Card.Img variant="top" src={image} />
+      <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        <Card.Text>{price}</Card.Text>
+      </Card.Body>
+      <ListGroup className="list-group-flush">
+        <ListGroupItem>Stock: {stock}</ListGroupItem>
+        <ListGroupItem>
+          {moment(published_date).format("DD-MM-YYYY")}
+        </ListGroupItem>
+        <ListGroupItem>{author}</ListGroupItem>
+        <ListGroupItem>{genre}</ListGroupItem>
+      </ListGroup>
+      <Card.Footer>
+        <Button variant="primary">Add to cart</Button>
+      </Card.Footer>
+    </Card>
+  );
+};
+export default CCard;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,34 @@
 import React from "react";
-import { NavBar } from "../components";
+import { NavBar, CCard } from "../components";
+import books from "../dataset/book_set.json";
+import { Container, Col, Row } from "react-bootstrap";
+import styled from "styled-components";
 
 function Home() {
   return (
     <div className="App">
       <NavBar />
+      <br />
+      <Container>
+        <Row>
+          <Col sm={9}>
+            <CardContainer>
+              {books.map((book) => {
+                return <CCard data={book} />;
+              })}
+            </CardContainer>
+          </Col>
+          <Col>2 of 2</Col>
+        </Row>
+      </Container>
     </div>
   );
 }
+
+const CardContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
 
 export default Home;
