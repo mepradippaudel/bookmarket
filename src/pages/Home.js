0b56f66bf03c5fdd8b01b89dd6ce0ba586a0e222
Pradip Paudel <pradippaudel2052@gmail.com>
2021-04-10 11:28:58 +0545

    calculate total  amount in cart

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,29 @@
 import React, { useContext, useState } from "react";
 import { NavBar, CCard } from "../components";
 import books from "../dataset/book_set.json";
-import { Container, Table, Col, Row } from "react-bootstrap";
+import { Container, Table, Col, Row, CarouselItem } from "react-bootstrap";
 import styled from "styled-components";
 import { Context } from "../providers";
 import { useAlert } from "react-alert";
 import CounterInput from "react-counter-input";
-import { convertDollerValueTORupee } from "../helpers";
+import { convertDollerValueTORupee, getTotalAmount } from "../helpers";
 
 function Home() {
   const [count, setCount] = useState(1);
   const alert = useAlert();
   const { state, dispatch } = useContext(Context);
+  const handleQuantity = (c, productId) => {
+    let findProduct = state.cart.find((cc) => cc.id === productId);
+    console.log(findProduct);
+    findProduct.quantity = c;
+    let findProductIndex = state.cart.findIndex((cc) => cc.id === productId);
+    const carts = state.cart;
+    carts[findProductIndex] = findProduct;
+    dispatch({
+      type: "carts",
+      carts: carts,
+    });
+  };
   const handleClickCart = (data) => {
     if (state.cart.length > 4) {
       alert.show("Sorry, you cann't add more than 5 books");
@@ -25,27 +37,31 @@ function Home() {
       },
     });
   };
+  console.log("STATE", state.cart);
   return (
     <div className="App">
       <NavBar />
       <br />
-      <Container>
-        <Row>
-          <Col sm={7}>
-            <CardContainer>
-              {books.map((book) => {
-                return (
-                  <CCard
-                    setCount={setCount}
-                    handleClick={handleClickCart}
-                    data={book}
-                    disabled={count > book.stock}
-                  />
-                );
-              })}
-            </CardContainer>
-          </Col>
-          <Col sm={5}>
+
+      <Row>
+        <Col sm={7}>
+          <h3>All Products</h3>
+          <CardContainer>
+            {books.map((book) => {
+              return (
+                <CCard
+                  setCount={setCount}
+                  handleClick={handleClickCart}
+                  data={book}
+                  disabled={count > book.stock}
+                />
+              );
+            })}
+          </CardContainer>
+        </Col>
+        <Col sm={5}>
+          <h3>My Carts</h3>
+          {state.cart.length > 0 ? (
             <Table striped bordered hover>
               <thead>
                 <tr>
@@ -76,18 +92,33 @@ function Home() {
                           min={1}
                           count={c.quantity}
                           max={10}
-                          onCountChange={(count) => console.log(count)}
+                          onCountChange={(count) => handleQuantity(count, c.id)}
                         />
                       </td>
-                      <td>{convertDollerValueTORupee(c.price) * c.quantity}</td>
+                      <td>
+                        {(
+                          convertDollerValueTORupee(c.price) * c.quantity
+                        ).toFixed(2)}
+                      </td>
                     </tr>
                   );
                 })}
               </tbody>
+              <tfoot>
+                <tr>
+                  <th></th>
+                  <th></th>
+                  <th></th>
+                  <th>Total Amount</th>
+                  <th>{getTotalAmount(state.cart).toFixed(2)}</th>
+                </tr>
+              </tfoot>
             </Table>
-          </Col>
-        </Row>
-      </Container>
+          ) : (
+            <p>Cart is empty</p>
+          )}{" "}
+        </Col>
+      </Row>
     </div>
   );
 }
